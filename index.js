8b8cf7387ebbbf543c0d5fc93e05fc2a1757a1ec
chanpan <chanpan.nuttaphon@gmail.com>
2017-11-09 21:20:23 +0700

    How to call Python function from NodeJS

---
+++ index.js
@@ -0,0 +1,26 @@
+var spawn = require("child_process").spawn;
+
+setTimeout(function() {
+    console.log("OK");
+    Demo1();
+}, 2000);
+
+setTimeout(function() {
+    Demo2();
+}, 5000);
+
+function Demo1() { ////แบบไม่มี parameter 
+    var process = spawn('python', ["demo1.py"]);
+    process.stdout.on('data', function(chunk) {
+        var textChunk = chunk.toString('utf8'); // buffer to string
+        console.log(JSON.parse(textChunk));
+    });
+}
+
+function Demo2() { //แบบมี parameter 
+    var process = spawn('python', ["demo2.py", 'demo']);
+    process.stdout.on('data', function(chunk) {
+        var textChunk = chunk.toString('utf8'); // buffer to string
+        console.log(textChunk);
+    });
+}
